ac42a4403a66c9d1f4e5dd7bda905beb296a10f5
Vlad_Ozik <badiozik@gmial.com>
2017-11-20 12:30:20 +0300

    testing

---
+++ polls/serialize.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from .models import Choice, Question
+
+
+class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Question
+        fields = ('question_text','pub_date')
+
+
+class ChoiceSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Choice
+        fields = ('question', 'choice_text', 'votes')
